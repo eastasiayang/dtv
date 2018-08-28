cd61bd273844e2d0f6c72dd2035687aa650400d2
yadong.yang <yadong.yang@tcl.com>
2018-08-28 17:05:34 +0800

    add live tv

---
+++ app/src/main/java/com/mobile/dtv/ActivityLiveFullTV.java
@@ -0,0 +1,14 @@
+package com.mobile.dtv;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActivityLiveFullTV extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_live_full_tv);
+    }
+}
